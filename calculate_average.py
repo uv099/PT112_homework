a690523f87092771b60581ad1b2e435c9c4930af
uv099 <buianhduong2809@gmail.com>
2025-09-17 00:20:21 +0700

    Add files via upload
    [Bài tập 2] Viết chương trình tính trung bình 3 số

---
+++ calculate_average.py
@@ -0,0 +1,5 @@
+num_1 = float(input(f"Số thứ nhất:"))
+num_2 = float(input(f"Số thứ hai:"))
+num_3 = float(input(f"Số thứ ba:"))
+average = (num_1 + num_2 + num_3) / 3
+print (f"Trung bình của ba số trên là: {average} ")
