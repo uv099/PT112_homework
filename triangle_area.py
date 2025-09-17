4325572eea6b7d5a2225cd132512d228c5e64520
uv099 <buianhduong2809@gmail.com>
2025-09-17 11:06:38 +0700

    Add files via upload
    [Bài tập 2] Viết chương trình tính diện tích tam giác

---
+++ triangle_area.py
@@ -0,0 +1,7 @@
+# Nhập độ dài đáy và chiều cao của tam giác 
+base = float(input (f"Nhập độ dài đáy:"))
+height = float(input(f"Nhập chiều cao:"))
+
+# Tính diện tích 
+area = 1/2 * base * height
+print (f"Diện tích của hình tam giác là: {area}")
