c9fa1f55903df6288446de425016e62b18d0a28d
uv099 <buianhduong2809@gmail.com>
2025-09-17 10:58:15 +0700

    Add files via upload
    [Thực hành 2] Viết chương trình tính diện tích hình chữ nhật

---
+++ rectangle_area.py
@@ -0,0 +1,7 @@
+# Nhập chiều dài chiều rộng hình chữ nhật 
+leghth = float(input("Nhập chiều dài:"))
+width = float(input("Nhập chiều rộng:"))
+# Tính diện tích
+area = leghth * width
+# Kết quả 
+print (f"Diện tích của hình chữ nhật là: {area}")
