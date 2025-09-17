4500bbc21d8a4678a9ac4f2207fed6d161d0481e
uv099 <buianhduong2809@gmail.com>
2025-09-17 18:09:41 +0700

    Add files via upload
    [Bài thực hành] Tính tổng và trung bình của ba số

---
+++ toan_tu.py
@@ -0,0 +1,12 @@
+# Nhập 3 số 
+a = float(input("Nhập số thứ nhất:"))
+b = float(input("Nhập số thứ hai:"))
+c = float(input("Nhập số thứ ba:"))
+
+# Bước 2: Tính tổng 
+tong = a + b +c
+
+# Bước 3: Tính trung bình 
+trung_binh = tong / 3
+print("Tổng của 3 số trên là:", tong)
+print("Trung bình là:", trung_binh)
